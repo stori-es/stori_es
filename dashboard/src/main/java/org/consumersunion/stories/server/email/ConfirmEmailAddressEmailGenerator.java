1cd1d8ea86151a16c66f7a61ed523510e52d5294
Maxime Mériouma-Caron <maximemc@navigo.com>
2016-07-27 22:25:38 -0400

    TASK-1251 : Verification email should point back to self

--- dashboard/src/main/java/org/consumersunion/stories/server/email/ConfirmEmailAddressEmailGenerator.java
+++ dashboard/src/main/java/org/consumersunion/stories/server/email/ConfirmEmailAddressEmailGenerator.java
@@ -1,6 +1,8 @@
 package org.consumersunion.stories.server.email;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.servlet.http.HttpServletRequest;
 
 import org.consumersunion.stories.common.server.model.Verification;
 import org.consumersunion.stories.server.velocity.VelocityWrapper;
@@ -12,22 +14,35 @@ import org.springframework.stereotype.Component;
 public class ConfirmEmailAddressEmailGenerator {
     private static final String TEMPLATE = TemplatesLocation.emails("emailadded.vm");
     private static final String SUBJECT = "[stori.es] Confirm your email address";
-    private static final String VERIFICATION_URL = "https://stori.es/confirm-email/";
+    private static final String VERIFICATION_URL = "/confirm-email/";
 
     private final VelocityWrapperFactory velocityWrapperFactory;
+    private Provider<HttpServletRequest> requestProvider;
 
     @Inject
-    ConfirmEmailAddressEmailGenerator(VelocityWrapperFactory velocityWrapperFactory) {
+    ConfirmEmailAddressEmailGenerator(
+            VelocityWrapperFactory velocityWrapperFactory,
+            Provider<HttpServletRequest> requestProvider) {
         this.velocityWrapperFactory = velocityWrapperFactory;
+        this.requestProvider = requestProvider;
     }
 
     public EmailData generate(Verification verification, String fullName) {
         VelocityWrapper velocityWrapper = velocityWrapperFactory.create(TEMPLATE);
 
-        velocityWrapper.put("confirmLink", VERIFICATION_URL + verification.getNonce());
+        String protocolAndHost = getProtocolAndHost();
+
+        velocityWrapper.put("confirmLink", protocolAndHost + VERIFICATION_URL + verification.getNonce());
 
         String body = velocityWrapper.generate();
 
         return new EmailData(SUBJECT, body, verification.getEmail(), fullName);
+    }
+
+    private String getProtocolAndHost() {
+        HttpServletRequest request = requestProvider.get();
+        StringBuffer url = request.getRequestURL();
+
+        return url.substring(0, url.indexOf(request.getRequestURI()));
     }
 }
