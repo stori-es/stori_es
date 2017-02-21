3801ac3b7047045b4a54254b8b99eeeab8b21017
Maxime Mériouma-Caron <maximemc@navigo.com>
2017-02-21 15:23:12 -0500

    TASK-2091 : Track User Last Sign-in to the stori.es Dashboard

--- dashboard/src/main/java/org/consumersunion/stories/server/security/authentication/SessionAuthenticationStrategy.java
+++ dashboard/src/main/java/org/consumersunion/stories/server/security/authentication/SessionAuthenticationStrategy.java
@@ -4,9 +4,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.consumersunion.stories.server.persistence.UserPersister;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.session.SessionInformation;
 import org.springframework.security.core.session.SessionRegistry;
@@ -17,18 +19,24 @@ import com.google.common.base.Strings;
 
 public class SessionAuthenticationStrategy extends ConcurrentSessionControlStrategy {
     private final SessionRegistry sessionRegistry;
+    private final UserPersister userPersister;
     private final Map<String, Authentication> connectedSessions;
 
-    public SessionAuthenticationStrategy(SessionRegistry sessionRegistry) {
+    @Inject
+    public SessionAuthenticationStrategy(
+            SessionRegistry sessionRegistry,
+            UserPersister userPersister) {
         super(sessionRegistry);
 
         this.sessionRegistry = sessionRegistry;
+        this.userPersister = userPersister;
         connectedSessions = new HashMap<String, Authentication>();
     }
 
     @Override
     public void onAuthentication(Authentication authentication, HttpServletRequest request,
             HttpServletResponse response) {
+        String username = authentication.getPrincipal().toString();
         if (sessionRegistry.getAllPrincipals().contains(authentication.getPrincipal())
                 && Strings.isNullOrEmpty(request.getHeader("renew"))) {
             throw new SessionAuthenticationException("Multiple login attempt!");
@@ -38,10 +46,13 @@ public class SessionAuthenticationStrategy extends ConcurrentSessionControlStrat
             if (sessions.size() > 0) {
                 SessionInformation sessionInformation = sessions.get(0);
                 sessionInformation.expireNow();
-                connectedSessions.get(authentication.getPrincipal().toString()).setAuthenticated(false);
+                connectedSessions.get(username).setAuthenticated(false);
             }
         }
-        connectedSessions.put(authentication.getPrincipal().toString(), authentication);
+
+        userPersister.updateLastLogin(username);
+
+        connectedSessions.put(username, authentication);
         super.onAuthentication(authentication, request, response);
     }
 }
